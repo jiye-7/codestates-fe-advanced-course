7ee301c6ffdf176489bdc644cdd7cdbaed7344eb
jiye-7 <jy522hi@gmail.com>
2022-08-01 21:02:05 +0900

    add: 100개 post목록 가져와서 화면에 렌더링

--- src/App.tsx
+++ src/App.tsx
@@ -1,14 +1,47 @@
-import React from 'react';
+import React, { useEffect, useState } from 'react';
 import './App.css';
+import axios, { AxiosResponse } from 'axios';
 
-function App() {
-  return (
-    <div className='App'>
-      <header className='App-header'>
-        <p>게시물 프로젝트</p>
-      </header>
-    </div>
-  );
+interface Post {
+	userId: number;
+	id: number;
+	title: string;
+	body: string;
+}
+
+function App(): JSX.Element {
+	const [posts, setPosts] = useState<Post[]>([]);
+
+	useEffect(() => {
+		const allPosts = async (): Promise<void> => {
+			try {
+				const { data }: AxiosResponse<Post[]> = await axios.get('https://jsonplaceholder.typicode.com/posts');
+
+				if (data) {
+					setPosts(data);
+				}
+			} catch (err) {
+				console.log(err);
+			}
+		};
+		allPosts();
+	}, []);
+
+	return (
+		<div className="App">
+			<header className="App-header">
+				<p>게시물 프로젝트</p>
+				<div>
+					{posts?.map(({ id, title, body }: Post) => (
+						<div key={id}>
+							<h2>{title}</h2>
+							<p>{body}</p>
+						</div>
+					))}
+				</div>
+			</header>
+		</div>
+	);
 }
 
 export default App;
