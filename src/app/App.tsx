6e38a91c0779cb7932071a9d1e20ab4e525ac2cf
jiye-7 <jy522hi@gmail.com>
2022-08-03 01:48:58 +0900

    fix: 게시글 클릭 시 토글로 body내용, 댓글 보여주도록 수정;

--- src/app/App.tsx
+++ src/app/App.tsx
@@ -2,7 +2,6 @@ import React from 'react';
 import { Routes, Route } from 'react-router-dom';
 import Navbar from 'components/Navbar/Navbar';
 import Posts from 'components/Posts/Posts';
-import PostDetail from 'components/PostDetail/PostDetail';
 
 function App(): JSX.Element {
 	return (
@@ -11,7 +10,6 @@ function App(): JSX.Element {
 			<main className="App">
 				<Routes>
 					<Route path="/" element={<Posts />} />
-					<Route path="/posts/:id" element={<PostDetail />} />
 				</Routes>
 			</main>
 		</>
--- src/types/PostInterface.ts
+++ src/types/PostInterface.ts
@@ -2,7 +2,7 @@ export interface IPostInterface {
 	userId: number;
 	id: number;
 	title: string;
-	body?: string;
+	body: string;
 	page?: string;
 }
 
