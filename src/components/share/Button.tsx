fbd9d2fa5cefaa893db7d45bb340ce7a3b0b9fd7
jiye-7 <jy522hi@gmail.com>
2022-08-05 17:55:51 +0900

    fix: button style / modify: README.md

--- src/components/share/Button.tsx
+++ src/components/share/Button.tsx
@@ -14,20 +14,18 @@ const Button = styled.button`
 	padding: 1rem;
 	border-radius: 5px;
 	margin-right: 0.5rem;
-
 	&:hover {
-		color: white;
-		background-color: #9fa8da;
-	}
-
-	&:focus {
 		color: white;
 		background-color: #9fa8da;
 	}
 `;
 
-const PageButton = ({ pageNumber, handlePagination }: IProps): JSX.Element => {
-	return <Button onClick={() => handlePagination(pageNumber)}>{pageNumber}</Button>;
+const PageButton = ({ pageNumber, currentPage, handlePagination }: IProps): JSX.Element => {
+	return (
+		<Button className={pageNumber === currentPage ? 'button-active' : ''} onClick={() => handlePagination(pageNumber)}>
+			{pageNumber}
+		</Button>
+	);
 };
 
 export default PageButton;
