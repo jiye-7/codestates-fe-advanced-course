bf047e7f34cc8f68d899cfb75a6871ed55d912d5
jiye-7 <jy522hi@gmail.com>
2022-08-05 11:15:16 +0900

    fix: 파일, 폴더구조 정리

--- src/app/App.tsx
+++ src/app/App.tsx
@@ -1,8 +1,8 @@
 import React from 'react';
 import { Routes, Route } from 'react-router-dom';
 import Navbar from 'components/Navbar/Navbar';
-import Posts from 'components/Posts/Posts';
-import PostDetail from 'components/PostDetailComments/PostDetailComments';
+import Posts from 'components/Post/Posts';
+import PostDetail from 'components/Post/PostDetail';
 
 function App(): JSX.Element {
 	return (
